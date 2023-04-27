6b44486f3cfde4687c8e62fda94e589f370c5b16
NguyenMinhMinh931903 <NguyenMinhMinh931903@gmail.com>
2023-04-27 02:36:34 -0400

    Add new files

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd /home/jenkins/test_code
+
+# Xóa các file đã biên dịch trước đó (nếu có)
+rm -rf build
+
+# Tạo thư mục build và điều hướng đến thư mục đó
+mkdir build
+cd build
+
+# copy file
+#cp ../CmakeList.txt .
+#cp ../hello.h .
+#cp ../hello.cpp .
+#cp ../main.h .
+
+# Tạo file Makefile từ file CMakeLists.txt
+if ! cmake ..; then
+   echo "CMake faild to generate Makefile"
+   exit 1
+fi
+
+# Biên dịch chương trình
+if ! make; then
+   echo"failed to build the program"
+   exit 1
+fi
+
+# Chạy chương trình
+./hello
