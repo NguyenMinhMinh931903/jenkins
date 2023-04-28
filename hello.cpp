099c46ad60949795b94bfc13d45a84cb95d9ef33
Nguyen Minh Minh <nguyenminhminh93bk@gmail.com>
2023-04-28 09:41:33 +0900

    28/4 2

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,5 @@
 #include "hello.h"
 
 std::string get_hello_message() {
-    return "Hello World! I am Minh ubuntu add Jenkins add 24/8";
+    return "Hello World! I am Minh ubuntu add Jenkins add 24/8 2";
 }
